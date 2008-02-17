384fe030b33a0d9a049f5cdbef5e9e49e7b0c832
emilioplatzer <emilioplatzer@7572296a-4846-0410-a662-c3d600aa1d73>
2008-02-17 02:56:15 +0000

    Pasa las pruebas de expandirSignoIgual

--- Pruebas.cs
+++ Pruebas.cs
@@ -25,6 +25,16 @@ namespace Mail2Access
 		[Test]
 		public void signoIgual(){
 			Assert.AreEqual("hola che",Otras.expandirSignoIgual("hola=20che"));
+			Assert.AreEqual("Región",Otras.expandirSignoIgual("Regi=F3n"));
+			Assert.AreEqual("Línea",Otras.expandirSignoIgual("L=EDnea"));
+			// Assert.AreEqual("el \nsalto",Otras.expandirSignoIgual("el =\nsalto"));
+			Assert.AreEqual("lang=ES",Otras.expandirSignoIgual("lang=3DES"));
+			/*
+			Assert.AreEqual("hola che",Otras.expandirSignoIgual("L=EDnea"));
+			Assert.AreEqual("hola che",Otras.expandirSignoIgual("L=EDnea"));
+			Assert.AreEqual("hola che",Otras.expandirSignoIgual("L=EDnea"));
+			Assert.AreEqual("hola che",Otras.expandirSignoIgual("L=EDnea"));
+			*/
 		}
 		[Test]
 		public void CreacionMdb(){
